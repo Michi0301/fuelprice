5502384fe14c5490ea53b28d2da047ff33073e85
Michael <fraundorfer.m@gmail.com>
2018-01-14 17:46:41 +0100

    Adds express js.

--- app.js
+++ app.js
@@ -1,8 +1,29 @@
 const PriceLookup = require("./app/price_lookup").PriceLookup;
-const location = "München",
-      radius = "5",
-      fuel = "super"
+const Cheapest = require("./app/finders/cheapest").Cheapest;
+let express = require('express');
+let app = express();
 
-new PriceLookup(fuel, location, radius).execute().then((stations) => {
-  console.log(new Finder(stations).find());
-})
+app.get('/stations', function (req, res) {
+  let location = req.query.location;
+  let radius = req.query.radius;
+  let fuel = req.query.fuel;
+
+  new PriceLookup(fuel, location, radius).execute().then((stations) => {
+    res.json(stations);
+  })
+});
+
+app.get('/cheapest', function (req, res) {
+  let location = req.query.location;
+  let radius = req.query.radius;
+  let fuel = req.query.fuel;
+
+  new PriceLookup(fuel, location, radius).execute().then((stations) => {
+    let cheapest = new Cheapest(stations).find();
+    res.json(cheapest);
+  })
+});
+
+app.listen(3000, function () {
+  console.log('Fuelprice app listening on port 3000!');
+});
